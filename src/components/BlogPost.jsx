d15f00a7680a5d048fd31e2f54fc8833333867dc
ablayev1999 <ablayev1999@gmail.com>
2026-03-16 10:43:05 +0500

    feat: add dynamic SEO meta tags to blog posts

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,9 +1,11 @@
-import { useParams, Link, Navigate } from 'react-router-dom';
+import { useParams, Link, Navigate, useLocation } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import { posts } from '../data/posts';
 import './Blog.css';
 
 function BlogPost({ lang }) {
     const { slug } = useParams();
+    const location = useLocation();
     const post = posts.find(p => p.slug === slug);
 
     if (!post) {
@@ -37,9 +39,30 @@ function BlogPost({ lang }) {
     };
 
     const data = getPostContent();
+    const fullUrl = `https://typer.uz${location.pathname}`;
 
     return (
         <div className="blog-container">
+            <Helmet>
+                <title>{`${data.title} | Typer.uz`}</title>
+                <meta name="description" content={data.description} />
+                <link rel="canonical" href={fullUrl} />
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="article" />
+                <meta property="og:url" content={fullUrl} />
+                <meta property="og:title" content={data.title} />
+                <meta property="og:description" content={data.description} />
+                <meta property="og:image" content="https://typer.uz/og-image.png" />
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={fullUrl} />
+                <meta property="twitter:title" content={data.title} />
+                <meta property="twitter:description" content={data.description} />
+                <meta property="twitter:image" content="https://typer.uz/og-image.png" />
+            </Helmet>
+
             <article className="post-article">
                 <div className="post-meta">
                     <Link to={data.backPath} className="back-to-blog">
